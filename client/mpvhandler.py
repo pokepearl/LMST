c4a8621b93a787bd0d86adcb3a9d699319f789bd
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-13 16:42:02 +1000

    Add code to display message after two seconds if MPV hasn't started up

--- client/mpvhandler.py
+++ client/mpvhandler.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 import subprocess
+import time
 def startmpv():
     print('Starting MPV Library...')
     subprocess.Popen('mpv --idle --input-ipc-server=/tmp/LMST-MPV-socket', shell=True, encoding='UTF-8')
+    time.sleep(2)
+    pcmd = subprocess.Popen(['/bin/bash', '-c', 'echo \'{ "command": ["client_name"] }\' | socat - /tmp/LMST-MPV-socket'], shell=False, stdout=subprocess.PIPE)
+    if '"Connection refused' not in pcmd.communicate()[0].decode('utf-8'):
+        pass
+    print('MPV Should be Online, if you see \'Connection Refused\' try reinstalling MPV')
