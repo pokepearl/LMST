f5c156ba45050b81b05485bcfa3443c33e29bc6f
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-13 11:40:22 +1000

    Separate MPV-specific code into separate file

--- client/lmst.py
+++ client/lmst.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import cmd
+from mpvhandler import *
 # Setup CMD commands
 class LMSTShell(cmd.Cmd):
     intro = 'Welcome to LMST. Type \'help\' for a list of commands'
     prompt = '(LMST) '
     def do_play(self,arg):
+        exampleT()
         pass
 
 if __name__ == '__main__':
