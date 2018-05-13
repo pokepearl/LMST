2888cecfe5aad7c6a480f6c34513b5471d773e9c
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-13 11:34:43 +1000

    Start implementing basic Cmd environment

--- client/lmst.py
+++ client/lmst.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 import cmd
+# Setup CMD commands
+class LMSTShell(cmd.Cmd):
+    def do_play(self,arg):
+        pass
+
+if __name__ == '__main__':
+    LMSTShell().cmdloop()
