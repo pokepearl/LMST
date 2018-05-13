a01f8582921009c72086be317347433ffdc5fd03
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-13 11:36:15 +1000

    Change prompt and intro message

--- client/lmst.py
+++ client/lmst.py
@@ -2,6 +2,8 @@
 import cmd
 # Setup CMD commands
 class LMSTShell(cmd.Cmd):
+    intro = 'Welcome to LMST. Type \'help\' for a list of commands'
+    prompt = '(LMST) '
     def do_play(self,arg):
         pass
 
