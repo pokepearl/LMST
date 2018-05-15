aa8a32658be72803d656b0b559c70e3c8afb28ea
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-15 11:31:55 +1000

    Add help documentation for Cmd functions/commands

--- client/lmst.py
+++ client/lmst.py
@@ -7,26 +7,37 @@ from confighandler import *
 class LMSTShell(cmd.Cmd):
     intro = 'Welcome to LMST. Type \'help\' for a list of commands'
     prompt = '(LMST) '
-
     def emptyline(self):
-        """Called when an empty line is entered in response to the prompt.
-        If this method is not overridden, it repeats the last nonempty
-        command entered.
-        """
         if self.lastcmd:
             self.lastcmd = ""
             return self.onecmd('\n')
     def do_play(self,arg):
+        """NOT YET IMPLEMENTED"""
         pass
     def do_quit(self, arg):
+        """
+        Exit LMST. Safely quits MPV and shuts down.
+        """
         stopmpv()
         time.sleep(3)
         raise SystemExit(0)
     def do_pause(self,args):
+        """
+        Pause Current Playback
+        """
         mpv_pause()
     def do_resume(self,args):
+        """
+        Pause Current Playback
+        """
         mpv_resume()
     def do_playdebug(self,args):
+        """
+        DEBUG COMMAND:
+        Play a file from a HTTP link.
+        Format:
+        * playdebug [link]
+        """
         http = args.split()
         mpv_playhttp(http[0])
 
