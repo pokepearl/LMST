ce76121b8544076eabf147f15b9bfdf76b8231bb
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-16 12:46:01 +1000

    Add Config Writer

--- client/confighandler.py
+++ client/confighandler.py
@@ -18,3 +18,11 @@ def readconfig(section,field):
         return config[section][field]
     except KeyError:
         return 'ERROR: Invalid Config Section or Field'
+def writeconfig(section,field,value):
+    config.read('lmstconf.ini')
+    try:
+        config[section][field] = value
+    except KeyError:
+        return 'ERROR: Invalid Config Section or Field'
+    with open('lmstconf.ini', 'w') as configfile:
+        config.write(configfile)
