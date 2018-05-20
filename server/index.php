adaf7109093ff1ac29ccaa2f9806e31df57a19c7
Alex Switala <aswiz123@hotmail.com>
2018-05-20 03:59:04 +0000

    Wrote HTML tables for the Index Page.

---
+++ server/index.php
@@ -0,0 +1,74 @@
+<html lang="en">
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+h1 {
+  text-align: center;
+  vertical-align: middle;
+}
+table {
+  vertical-align: middle;
+}
+
+</style>
+
+<title>Example - LMST</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
+<body>
+<!--Ignore the below php section, this imports the header file for the navbar -->
+<?php
+include 'header.php';
+?>
+<main role="main" class="container mx-auto">
+<!-- This Section is Centred, place your code in here -->
+<br>
+<br>
+<br>
+<h1> Welcome to LMST </h1>
+<br>
+<div class="Index-Table">
+<table style="width:70%" class="table" align="center">
+		<tr>
+		<td width="25%">Connection Info:</td>
+		<td>Placeholder</td> 
+		</tr>
+  
+		<tr>
+		<td>URL:</td>
+		<td>Placeholder</td> 
+		</tr>
+	
+		<tr>
+		<td>Folder:</td>
+		<td>Placeholder</td> 
+		</tr>
+
+		<tr>
+		<td>DataBase:</td>
+		<td>Placeholder</td> 
+		</tr>
+</table>
+</div>
+</main>
+<!-- Bootstrap 4 JS Files -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</body>
+</html>
