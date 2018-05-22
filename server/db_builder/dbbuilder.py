d46b67c9333d8513cd8124005d659e9961be4299
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-22 10:20:31 +1000

    Glob files in subdirectory and append to list

--- server/db_builder/dbbuilder.py
+++ server/db_builder/dbbuilder.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 import sqlite3
+import os
 import formic # This is a special Python 3 compatible version of Formic
+# Checking root folder, this should be rewritten
+dircheck = input('Is this script being run from the root directory of the LMST webserver? (yes/no): ')
+if dircheck == "yes":
+    print('Continuing...')
+elif dircheck == "no":
+    print('Exiting, rerun from the webserver root')
+    raise SystemExit(1)
+else:
+    print('Invalid response, Exiting')
+    raise SystemExit(1)
+# Glob all files with approved extensions
+currentdir = os.getcwd()+'/Music/' #Default Music Directory
+muspattern = ["*.mp3", "*.ogg", "*.opus"] # Default Extensions
+filelist = formic.FileSet(directory=currentdir, include=muspattern)
+musiclist = [] #Predefine Music List
+for filename in filelist.qualified_files():
+    musiclist.append(filename)
