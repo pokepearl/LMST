39bc387206b8910a7ec110191e0e9bf8fee4865e
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-22 11:17:36 +1000

    Start Scraping metadata

--- server/db_builder/dbbuilder.py
+++ server/db_builder/dbbuilder.py
@@ -4,6 +4,7 @@ import os
 import formic # This is a special Python 3 compatible version of Formic
 import mutagen
 import subprocess
+import re
 # Checking root folder, this should be rewritten
 dircheck = input('Is this script being run from the root directory of the LMST webserver? (yes/no): ')
 if dircheck == "yes":
@@ -27,3 +28,9 @@ connect = sqlite3.connect('lmst.db')
 cursor = connect.cursor()
 cursor.execute('DROP TABLE IF EXISTS songs')
 cursor.execute('CREATE TABLE songs (title text,artist text,album text,length text)')
+for fname in musiclist:  # Start Scraping info from songs and append to DB
+    parsefile = subprocess.Popen(["mutagen-inspect", fname], stdout=subprocess.PIPE) # Read File metadata
+    greptitle = subprocess.Popen(["grep","Title"], stdin=parsefile.stdout,stdout=subprocess.PIPE)
+    filetitle = greptitle.communicate()[0]
+    filetitle = re.sub("Title=",'',filetitle.decode('UTF-8'))
+    print(filetitle)
