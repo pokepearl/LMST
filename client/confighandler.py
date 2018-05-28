571d411f7bd269de01c63ddfc5835eba019ae16f
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-28 16:53:18 +1000

    Switch to appdirs to store configs

--- client/confighandler.py
+++ client/confighandler.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 import configparser
 import os
+from appdirs import *
 config = configparser.ConfigParser()
+confpath = user_config_dir('LMST')
 def createconfig():
     config = configparser.ConfigParser()
-    if os.path.isfile('lmstconf.ini') == True:
+    if os.path.isfile(confpath+'/lmstconf.ini') == True:
         pass
     else:
         config['HTTP'] = {'host': 'http://',
                           'dbdirectory': '/cache/',
                           'dbname': 'lmst.db'}
         config['FILE'] = {'localdb': 'files/lmst.db'}
-        with open('lmstconf.ini', 'w') as configfile:
+        with open(confpath+'/lmstconf.ini', 'w') as configfile:
             config.write(configfile)
 def readconfig(section,field):
-    config.read('lmstconf.ini')
+    config.read(confpath+'/lmstconf.ini')
     try:
         return config[section][field]
     except KeyError:
         return 'ERROR: Invalid Config Section or Field'
 def writeconfig(section,field,value):
-    config.read('lmstconf.ini')
+    config.read(confpath+'/lmstconf.ini')
     try:
         config[section][field] = value
     except KeyError:
         return 'ERROR: Invalid Config Section or Field'
-    with open('lmstconf.ini', 'w') as configfile:
+    with open(confpath+'/lmstconf.ini', 'w') as configfile:
         config.write(configfile)
