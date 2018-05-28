f510a5cad14d30508b1a342ffb693dd28dedaa9e
sxsg430 <872597+sxsg430@users.noreply.github.com>
2018-05-28 16:50:42 +1000

    Remove params which can potenially cause issues

--- client/mpvhandler.py
+++ client/mpvhandler.py
@@ -5,7 +5,7 @@ import os
 FNULL = open(os.devnull, 'w')
 def startmpv():
     print('Starting MPV Library...')
-    subprocess.Popen('mpv --idle --no-audio-display --no-video --no-input-terminal --input-ipc-server=/tmp/LMST-MPV-socket', shell=True, encoding='UTF-8',stdin=FNULL, stderr=FNULL, stdout=FNULL)
+    subprocess.Popen('mpv --idle --no-audio-display --no-video --no-input-terminal --input-ipc-server=/tmp/LMST-MPV-socket', shell=True,stdin=FNULL, stderr=FNULL, stdout=FNULL)
     time.sleep(2)
     pcmd = subprocess.Popen(['/bin/bash', '-c', 'echo \'{ "command": ["client_name"] }\' | socat - /tmp/LMST-MPV-socket'], shell=False, stdout=subprocess.PIPE)
     if '"Connection refused' not in pcmd.communicate()[0].decode('utf-8'):
@@ -31,5 +31,5 @@ def mpv_reset():
     time.sleep(1)
     subprocess.Popen(
         'mpv --idle --no-audio-display --no-video --no-input-terminal --input-ipc-server=/tmp/LMST-MPV-socket',
-        shell=True, encoding='UTF-8', stdin=FNULL, stderr=FNULL, stdout=FNULL)
+        shell=True, stdin=FNULL, stderr=FNULL, stdout=FNULL)
     time.sleep(2)
